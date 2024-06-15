37964d8c2a220b4ffb904cc9e0e2b91519439dd3
Alejandro Ponce <122680586+alejandroponce00@users.noreply.github.com>
2024-06-14 21:02:32 -0300

    agregado de carrusel4

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body className={`${zeyada.className} antialiased `}><Nav />{children}</body>
+      <body className={`${zeyada.className} antialiased `}><Nav />{children}
+      <footer className='py-10 flex justify-center items-center text-2xl'>
+        creado por Alejandro Ponce
+
+        </footer></body>
     </html>
   );
 }
