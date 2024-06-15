1870f218d16d640a4967375b96df1a60937c370f
Alejandro Ponce <122680586+alejandroponce00@users.noreply.github.com>
2024-06-15 16:56:18 -0300

    correcciones

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,26 @@
 import Card from "./components/card";
 
+import { roboto } from "@/ui/fonts";
+
 export default function Home() {
   return (
     <div>
       <h1 className=" text-center text-7xl text-gray-500 underline">
         Ojos de Cielo Arte
       </h1>
+      <div>
+        <p className={`${roboto.className}`}>¡Bienvenidos a mi mundo de arte y creatividad!
+
+Soy una artista especializada en transformar materiales comunes en obras únicas y llenas de vida. A través de técnicas de modelado en porcelana fría y la pintura sobre diversos soportes, doy vida a piezas exclusivas que reflejan mi pasión y dedicación por el arte.<br/>
+
+<strong className="text-center"><u>¿Qué encontrarás aquí?</u></strong> <br/>
+Diseños en porcelana fría: Cada pieza es cuidadosamente modelada a mano sobre vidrio y madera, creando decoraciones detalladas y únicas.
+Pinturas acrílicas sobre madera: Mis cuadros son una explosión de color y emoción, pintados con esmero para embellecer cualquier espacio.
+Accesorios personalizados: Desde aritos y llaveros hasta discos de vinilo pintados a mano, cada artículo es una obra de arte que puedes llevar contigo.
+Cada creación es el resultado de un proceso meticuloso y apasionado, diseñado para aportar belleza y originalidad a tu vida diaria. Explora mi galería y descubre cómo el arte puede transformar lo cotidiano en extraordinario.
+
+¡Gracias por visitar mi sitio web!</p>
+      </div>
       <div className="flex content-center mx-auto m-2">
         <Card
           direccion="/imagenes/colibri.webp"
--- src/ui/fonts.jsx
+++ src/ui/fonts.jsx
@@ -1,9 +1,13 @@
-import {Zeyada } from 'next/font/google'
-import { Lusitana } from 'next/font/google'
-
-export const zeyada = Zeyada({ subsets:['latin'],weight:'400',})
+import { Zeyada } from "next/font/google";
+import { Lusitana } from "next/font/google";
+import { Roboto } from "next/font/google";
+export const zeyada = Zeyada({ subsets: ["latin"], weight: "400" });
 
 export const lusitana = Lusitana({
-    weight:['400','700'],
-    subsets:['latin']
-})
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
+export const roboto = Roboto({
+    weight: ["400", "700"],
+    subsets: ["latin"],
+  });
