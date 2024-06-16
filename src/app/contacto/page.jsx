efacf68825bd7fcd4736ac208def905defa963c2
Alejandro Ponce <122680586+alejandroponce00@users.noreply.github.com>
2024-06-15 21:21:18 -0300

    correccione11

--- src/app/contacto/page.jsx
+++ src/app/contacto/page.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import Image from 'next/image'
 function Contacto() {
   return (
-    <div className='my-7 text-center'>
+    <div className='my-7 text-center px-auto'>
+    <Image
+      
+      src="/imagenes/9-removebg-preview.png "
+      alt="Picture of the author"
+      width={500}
+      height={500}
+      className='opacity-70 items-center'
+    />
     <a
 href="https://www.instagram.com/ojosdecielo.arte"
 className="rounded-md bg-red-500 px-3 py-2  font-medium text-white text-2xl"
--- src/app/porcelana/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Porcelana() {
-  return (
-    <div><h1 className='text-center font-bold text-4xl'>Porcelana Fria</h1></div>
-  )
-}
-
-export default Porcelana 
