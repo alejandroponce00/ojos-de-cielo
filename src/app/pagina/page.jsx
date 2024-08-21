a715b8f7dc6be825e8658892389ad970b079e265
Alejandro Ponce <122680586+alejandroponce00@users.noreply.github.com>
2024-08-21 19:55:10 -0300

    cambio en slider gaaleria

---
+++ src/app/pagina/page.jsx
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react'
+
+import ParticlesBackground from '../components/config/particlesBackground'
+
+function Pagina() {
+  return (
+    <ParticlesBackground />
+  )
+}
+
+export default Pagina
